b16bf3d7337160fbe34c7c72f958498d8b182167
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-01-30 22:00:07 -0300

    devCommit

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const pacientes = [
+  {id: 1, nome: 'João', idade: 15},
+  {id: 2, nome: 'Maria', idade: 20},
+  {id: 3, nome: 'Luiz', idade: 25},
+];
+
 exports.handler = async (event) => {
+  const { pacienteId } = event;
+  const pacienteEcontrado = pacientes.find(
+    paciente => paciente.id == pacienteId
+  );
+  
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello world")
+    body: JSON.stringify(pacienteEcontrado)
   }
 
   return response;
